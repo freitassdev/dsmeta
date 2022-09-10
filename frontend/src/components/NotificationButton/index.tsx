edf3b692551106a791bbd8fdb74f81ef8c03188b
Michel Freitas <chelmizinho@gmail.com>
2022-09-10 15:41:36 -0300

    Implementando o Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,13 +2,14 @@ import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/requests";
 import './styles.css';
 import axios from 'axios';
+import { toast } from "react-toastify";
 type Props = {
     saleId: Number;
 }
 
 function handleClick(id :Number) {
     axios(`${BASE_URL}/sales/${id}/notification`).then(r => {
-        console.log("ENVIADO")
+        toast.info("SMS enviado com sucesso")
     })
 }
 function NotificationButton( {saleId} : Props ) {
