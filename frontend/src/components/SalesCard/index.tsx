243065bc1416ede5eaf74362d352e35a86a0e1ee
Michel Freitas <chelmizinho@gmail.com>
2022-09-10 14:21:41 -0300

    Implementação Axios

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton';
@@ -10,7 +11,11 @@ function SalesCard() {
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
-
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales").then(response => {
+            console.log(response.data)
+        });
+    }, []);
 
     return (
         <div className="dsmeta-card">
